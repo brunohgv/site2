a0ec60f42009ef8ed9a78716c2a358354a4a766b
Bruno Henrique Gusmão Vasconcelos <brunohgv2@gmail.com>
2018-01-24 02:23:05 -0300

    Acho que o contato está pronto

--- php/form.php
+++ php/form.php
@@ -1,22 +1,38 @@
 <?php
-	if (isset($_POST['submit'])) {
-		$name = $_POST['nome'];
-		$surname = $_POST['sobrenome'];
-		$email = $_POST['email'];
-		$phone = $_POST['telefone'];
-		$subject_form = $_POST['assunto'];
-		$msg = $_POST['msg'];
+	if (isset($_POST['nome']) && isset($_POST['sobrenome']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['assunto']) && isset($_POST['msg'])) {
+		if (!empty($_POST['nome']) && !empty($_POST['sobrenome']) && !empty($_POST['email']) && !empty($_POST['telefone']) && !empty($_POST['assunto']) && !empty($_POST['msg'])) {
+			$fname = $_POST['nome'];
+			$lname = $_POST['sobrenome'];
+			$email = $_POST['email'];
+			$phone = $_POST['telefone'];
+			$subject_form = $_POST['assunto'];
+			$msg = $_POST['msg'];
 
-		$to = 'brunohgv2@gmail.com';
-		$subject = 'Formulário Contato Site';
-		$message = "Nome: ".$name." ".$surname."\n"."Telefone: ".$phone."\n"."Email: ".$email."\n\n"."Assunto: ".$subject_form."\n\n".$msg;
-		$headers = "From: ".$email;
+			
 
-		if (mail($to, $subject, $message, $headers)) {
-			echo "<h1>Mensagem enviada com sucesso! Obrigado, ".$name." em breve entraremos em contato!</h1>";
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				echo "Email inválido";
+			} else {
+
+				$to = 'brunohgv2@gmail.com';
+				$subject = 'Formulário Contato Site';
+				$message = "Nome: ".$fname." ".$lname."\n"."Telefone: ".$phone."\n"."Email: ".$email."\n\n"."Assunto: ".$subject_form."\n\n".$msg;
+				$headers = "From: ".$email;
+
+				if (mail($to, $subject, $message, $headers)) {
+				echo "Mensagem enviada com sucesso! Obrigado, ".$name." em breve entraremos em contato!";
+				} else {
+				echo "Algo de errado aconteceu!";
+			}
+			}
+
+			
 		} else {
-			echo "<h1>Algo de errado aconteceu!</h1>";
+			echo "Todos os campos precisam ser preenchidos";
 		}
+		
+	} else {
+		echo "Falha na primeira verificaçao";
 	}
 
 ?>
